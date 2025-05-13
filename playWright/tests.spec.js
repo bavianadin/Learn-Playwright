97a10714fe3659dfd354a9269a0cb0593db0389d
bavianadin <bavianadin@gmail.com>
2025-05-13 15:33:56 +0700

    first commit

---
+++ playWright/tests.spec.js
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('First test', async ({page}) => {
+    await page.goto('https://google.com/');
+    await expect(page).toHaveTitle("Google");
+});
